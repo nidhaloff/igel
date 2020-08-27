fe89f8642410ac67993fedd3bdebcbca6d966888
= <nidhalbacc@gmail.com>
2020-08-27 23:09:56 +0200

    second commit

--- igel/__init__.py
+++ igel/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Nidhal Baccouri"""
 __email__ = 'nidhalbacc@gmail.com'
-__version__ = '0.0.1'
+__version__ = '0.0.2'
--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/nidhaloff/igel',
-    version='0.0.1',
+    version='0.0.2',
     zip_safe=False,
 )
