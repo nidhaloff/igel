552edeff7ae3004e963676c91f3e276477aa2cf7
= <nidhalbacc@gmail.com>
2020-08-30 21:15:50 +0200

    added logo

--- tests/test_igel.py
+++ tests/test_igel.py
@@ -1,32 +1,3 @@
 #!/usr/bin/env python
 
 """Tests for `igel` package."""
-
-import pytest
-
-
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
-
-
-def test_command_line_interface():
-    """Test the CLI."""
-    # runner = CliRunner()
-    # result = runner.invoke(cli.main)
-    # assert result.exit_code == 0
-    # assert 'igel.cli.main' in result.output
-    # help_result = runner.invoke(cli.main, ['--help'])
-    # assert help_result.exit_code == 0
-    # assert '--help  Show this message and exit.' in help_result.output
