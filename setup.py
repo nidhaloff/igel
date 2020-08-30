da4a6c88414d2835fa3cfa2295a90f27f1355d87
= <nidhalbacc@gmail.com>
2020-08-30 19:03:32 +0200

    added travis build

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open('HISTORY.rst') as history_file:
 with open('requirements.txt') as f:
     requirements = [req.strip() for req in f]
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = ['pytest-runner', *requirements]
 
 test_requirements = ['pytest>=3', ]
 
