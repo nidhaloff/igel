c72e33302f41c2257e2bbd4f31660770c656f5a8
= <nidhalbacc@gmail.com>
2020-09-05 16:55:29 +0200

    refactored code

--- igel/igel.py
+++ igel/igel.py
@@ -3,13 +3,19 @@
 import pandas as pd
 import pickle
 import os
-from pathlib import Path
-from igel.utils import read_yaml, extract_params, _reshape
-from igel.data import evaluate_model
-from igel.data import models_dict, metrics_dict
 import json
 import warnings
 import logging
+try:
+    from igel.utils import read_yaml, extract_params, _reshape
+    from igel.data import evaluate_model
+    from igel.configs import configs
+    from igel.data import models_dict, metrics_dict
+except ImportError:
+    from utils import read_yaml, extract_params, _reshape
+    from data import evaluate_model
+    from configs import configs
+    from data import models_dict, metrics_dict
 
 warnings.filterwarnings("ignore")
 logging.basicConfig(format='%(levelname)s - %(message)s', level=logging.INFO)
@@ -20,39 +26,37 @@ class IgelModel(object):
     """
     IgelModel is the base model to use the fit, evaluate and predict functions of the sklearn library
     """
-    commands = ('fit', 'evaluate', 'predict')
     model_types = ('regression', 'classification')
-    command = None
-    model_path = None
+    default_configs = configs
     model = None
-    target = None
-    features = None
-    stats_dir = 'model_results'
-    res_path = Path(os.getcwd()) / stats_dir
-    save_to = 'model.sav'
 
     def __init__(self, command: str, **cli_args):
-        logger.info(f"CLI args: { cli_args}")
-        logger.info(f"Command: { command}")
-        if command not in self.commands:
-            logger.exception(f"Please choose an existing command -> {self.commands}")
+        logger.info(f"Entered CLI args:         { cli_args}")
+        logger.info(f"Chosen command:           { command}")
+        self.data_path = cli_args.get('data_path')  # path to the dataset
 
-        self.command = command
-        self.data_path = cli_args.get('data_path')
+        if command == "fit":
+            self.yml_path = cli_args.get('yaml_path')
+            self.yaml_configs = read_yaml(self.yml_path)
+            logger.info(f"your chosen configuration: {self.yaml_configs}")
 
-        if self.command == "fit":
-            self.model_definition_file = cli_args.get('yaml_path')
-            self.yaml_config = read_yaml(self.model_definition_file)
-            logger.info(f"model configuration: {self.yaml_config}")
-
-            self.model_type, self.target, self.algorithm = extract_params(self.yaml_config)
+            # dataset options given by the user
+            self.dataset_props: dict = self.yaml_configs.get('dataset',
+                                                             self.default_configs['dataset_props'])
+            # model options given by the user
+            self.model_props: dict = self.yaml_configs.get('model',
+                                                           self.default_configs['model_props'])
+            # list of target(s) to predict
+            self.target: list = self.yaml_configs.get('target')
 
         else:
-            self.model_path = cli_args.get('model_path')
-            with open(self.res_path / 'description.json', 'r') as f:
+            default_model_path = self.default_configs['default_model_path']
+            self.model_path = cli_args.get('model_path', default_model_path)
+            logger.info(f"path of the pre-fitted model => {self.model_path}")
+            with open(self.default_configs['description_file'], 'r') as f:
                 dic = json.load(f)
-                self.target = dic.get("target")
-                self.model_type = dic.get("type")
+                self.target: list = dic.get("target")  # target to predict as a list
+                self.model_type: str = dic.get("type")  # type of the model -> regression or classification
 
     def _create_model(self, model_type: str, model_algorithm: str):
         """
@@ -69,47 +73,52 @@ class IgelModel(object):
         else:
             return model
 
-    def _save_model(self, model, save_to: str):
+    def _save_model(self, model):
         """
         save the model to a binary file
         @param model: model to save
-        @param save_to: path where to save the model
         @return: bool
         """
-        cwd = Path(os.getcwd())
-        self.res_path = cwd / self.stats_dir
+        res_path = self.default_configs['res_path']
+        stats_dir = self.default_configs['stats_dir']
+        model_path = self.default_configs['default_model_path']
+
         try:
-            if not os.path.exists(self.res_path):
-                os.mkdir(self.res_path)
+            if not os.path.exists(res_path):
+                os.mkdir(res_path)
             else:
-                logger.info(f"{self.stats_dir} already exists in the path")
-                logger.warning(f"data in the {self.stats_dir} folder will be overridden")
+                logger.info(f"{stats_dir} already exists in the path")
+                logger.warning(f"data in the {stats_dir} folder will be overridden. If you don't"
+                               f"want this, then move the current {stats_dir} to another path")
 
         except OSError:
-            logger.exception("Creation of the directory %s failed" % self.stats_dir)
+            logger.exception(f"Creation of the directory {stats_dir} failed ")
         else:
-            logger.info("Successfully created the directory %s " % self.stats_dir)
-            pickle.dump(model, open(self.res_path / save_to, 'wb'))
+            logger.info(f"Successfully created the directory {stats_dir} ")
+            pickle.dump(model, open(model_path, 'wb'))
             return True
 
-    def _load_model(self, f: str):
+    def _load_model(self, f: str=''):
         """
         load a saved model from file
         @param f: path to model
         @return: loaded model
         """
+        res_path = self.default_configs['res_path']
+        stats_dir = self.default_configs['stats_dir']
+        model_path = self.default_configs['default_model_path']
+
         try:
             if not f:
-                path = self.res_path / self.save_to
-                logger.info(f"result path: {self.res_path} | saved to: {self.save_to}")
-                logger.info(f"loading model form {path} ")
-                model = pickle.load(open(path, 'rb'))
+                logger.info(f"result path: {res_path}")
+                logger.info(f"loading model form {model_path} ")
+                model = pickle.load(open(model_path, 'rb'))
             else:
                 logger.info(f"loading from {f}")
                 model = pickle.load(open(f, 'rb'))
             return model
         except FileNotFoundError:
-            logger.error(f"File not found in {self.res_path / self.save_to}")
+            logger.error(f"File not found in {model_path}")
 
     def _prepare_fit_data(self):
         return self._process_data()
@@ -127,10 +136,11 @@ class IgelModel(object):
         try:
             dataset = pd.read_csv(self.data_path)
             logger.info(f"dataset shape: {dataset.shape}")
-            self.features = list(dataset.columns)
+            attributes = list(dataset.columns)
+            logger.info(f"dataset attributes: {attributes}")
 
-            if any(col not in self.features for col in self.target):
-                raise Exception("chosen targets to predict must exist in the dataset")
+            if any(col not in attributes for col in self.target):
+                raise Exception("chosen target(s) to predict must exist in the dataset")
 
             y = pd.concat([dataset.pop(x) for x in self.target], axis=1)
             x = _reshape(dataset.to_numpy())
@@ -158,26 +168,33 @@ class IgelModel(object):
         fit a machine learning model and save it to a file along with a description.json file
         @return: None
         """
-        model_class = self._create_model(self.model_type, self.algorithm)
+        model_type = self.model_props.get('type')
+        algorithm = self.model_props.get('algorithm')
+        if not model_type or not algorithm:
+            raise Exception(f"model_type and algorithm cannot be None")
+
+        logger.info(f"Solving a {model_type} problem using ===> {algorithm}")
+        model_class = self._create_model(model_type, algorithm)
         self.model = model_class()
         logger.info(f"executing a {self.model.__class__.__name__} algorithm ..")
         x, y = self._prepare_fit_data()
         self.model.fit(x, y)
-        saved = self._save_model(self.model, save_to=self.save_to)
+        saved = self._save_model(self.model)
         if saved:
-            logger.info(f"model saved successfully and can be found in the {self.stats_dir} folder")
+            logger.info(f"model saved successfully and can be found in the {self.default_configs['stats_dir']} folder")
 
         fit_description = {
             "model": self.model.__class__.__name__,
-            "type": self.model_type,
+            "type": self.model_props['type'],
+            "algorithm": self.model_props['algorithm'],
             "data_path": self.data_path,
-            "results_path": str(self.res_path),
-            "model_path": str(self.res_path),
+            "results_path": str(self.default_configs['res_path']),
+            "model_path": str(self.default_configs['res_path']),
             "target": self.target
         }
 
         try:
-            json_file = self.res_path / "description.json"
+            json_file = self.default_configs['description_file']
             logger.info(f"saving fit description to {json_file}")
             with open(json_file, 'w', encoding='utf-8') as f:
                 json.dump(fit_description, f, ensure_ascii=False, indent=4)
@@ -190,7 +207,7 @@ class IgelModel(object):
         @return: None
         """
         try:
-            model = self._load_model(f=self.model_path)
+            model = self._load_model()
             x_val, y_true = self._prepare_val_data()
             y_pred = model.predict(x_val)
             eval_results = evaluate_model(model_type=self.model_type,
@@ -198,7 +215,7 @@ class IgelModel(object):
                                           y_true=y_true,
                                           **kwargs)
 
-            eval_file = self.res_path / "evaluation.json"
+            eval_file = self.default_configs['evaluation_file']
             logger.info(f"saving fit description to {eval_file}")
             with open(eval_file, 'w', encoding='utf-8') as f:
                 json.dump(eval_results, f, ensure_ascii=False, indent=4)
@@ -221,9 +238,9 @@ class IgelModel(object):
             logger.info(f"predict on targets: {self.target}")
             df_pred = pd.DataFrame.from_dict({self.target[i]: y_pred[:, i] if len(y_pred.shape) > 1 else y_pred for i in range(len(self.target))})
 
-            path = self.res_path / 'predictions.csv'
-            logger.info(f"saving the predictions to {path}")
-            df_pred.to_csv(path)
+            pred_path = self.default_configs['prediction_file']
+            logger.info(f"saving the predictions to {pred_path}")
+            df_pred.to_csv(pred_path)
 
         except Exception as e:
             logger.exception(f"Error while preparing predictions: {e}")
@@ -231,8 +248,8 @@ class IgelModel(object):
 
 if __name__ == '__main__':
     mock_params = {'data_path': '/home/nidhal/my_projects/igel/examples/data/example.csv',
-                   'model_definition_file': '/home/nidhal/my_projects/igel/examples/model.yaml'}
+                   'yml_path': '/home/nidhal/my_projects/igel/examples/model.yaml'}
 
-    reg = IgelModel('fit', **mock_params)
-    reg.fit()
+    reg = IgelModel('predict', **mock_params)
+    # reg.fit()
     reg.predict()
