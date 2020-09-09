a7e1d6ef1ca8a20bb662a4e587432ca378ff9e2e
= <nidhalbacc@gmail.com>
2020-09-09 23:30:00 +0200

    updated version

--- setup.py
+++ setup.py
@@ -51,6 +51,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/nidhaloff/igel',
-    version="0.1.4",
+    version="0.1.5",
     zip_safe=False,
 )
