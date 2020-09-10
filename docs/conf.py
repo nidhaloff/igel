b963de2753eb1f87d1b79646e2f015c0fdcb41aa
= <nidhalbacc@gmail.com>
2020-09-10 21:35:56 +0200

    added html_theme_options in docs

--- docs/conf.py
+++ docs/conf.py
@@ -89,7 +89,23 @@ html_theme = 'classic'
 # theme further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'html_logo': 'https://github.com/nidhaloff/igel/blob/master/assets/logo.jpg',
+    'canonical_url': '',
+    'analytics_id': 'UA-XXXXXXX-1',  #  Provided by Google in your dashboard
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'white',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
