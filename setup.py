8b025b33406fb7a3f8ef112395a52f51868d61be
= <nidhalbacc@gmail.com>
2020-09-12 23:57:50 +0200

    implemented optional args

--- setup.py
+++ setup.py
@@ -51,6 +51,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/nidhaloff/igel',
-    version="0.1.6",
+    version="0.1.7",
     zip_safe=False,
 )
