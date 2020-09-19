4970e98695443317404084140ce6e16dd59b9985
nidhal baccouri <nidhalbacc@gmail.com>
2020-09-19 11:48:32 +0200

    fixed docs

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ import sys
 sys.path.insert(0, os.path.abspath('..'))
 
 import igel
-import sphinx_copybutton
+
 # -- General configuration ---------------------------------------------
 
 
@@ -33,7 +33,7 @@ html_logo = '../assets/logo.jpg'
 # needs_sphinx = '1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx-copybutton']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx_copybutton']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
