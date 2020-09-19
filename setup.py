3d4f7c55f9850b9e9a17ea0ea15011f472579ecb
nidhal baccouri <nidhalbacc@gmail.com>
2020-09-19 12:39:32 +0200

    added docs deps

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ requirements = [
 
 setup_requirements = ['pytest-runner']
 test_requirements = ['pytest>=3']
-docs_require = ['sphinx-copybutton']
+# docs_require = ['sphinx-copybutton']
 
 setup(
     author=__author__,
@@ -56,7 +56,6 @@ setup(
     name='igel',
     packages=find_packages(include=['igel', 'igel.*']),
     setup_requires=setup_requirements,
-    extras_require={'docs': docs_require},
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/nidhaloff/igel',
