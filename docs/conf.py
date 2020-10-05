122ed92211465dcf456b2dcff3d145439dfe4fd0
nidhal baccouri <nidhalbacc@gmail.com>
2020-10-05 19:51:30 +0200

    added internal ref to the readme

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,7 @@ html_logo = '../assets/logo.jpg'
 # needs_sphinx = '1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode'] # , 'sphinx_copybutton'
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.autosectionlabel'] # , 'sphinx_copybutton'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
