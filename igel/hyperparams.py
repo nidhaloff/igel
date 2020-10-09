3dcaf50de2b8c73a8db3ed3df21a21eca1e4fe7f
nidhal baccouri <nidhalbacc@gmail.com>
2020-10-09 22:42:02 +0200

    implemented hyperparm search

--- igel/hyperparams.py
+++ igel/hyperparams.py
@@ -1,6 +1,16 @@
 from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 
 
-def hyperparameter_search(method, **kwargs):
-    search = GridSearchCV(**kwargs) if method == 'grid_search' else RandomizedSearchCV(**kwargs)
+def hyperparameter_search(model,
+                          method,
+                          params,
+                          x_train,
+                          y_train,
+                          **kwargs):
+    search = GridSearchCV(model,
+                          params,
+                          **kwargs) if method == 'grid_search' else RandomizedSearchCV(model,
+                                                                                       params,
+                                                                                       **kwargs)
+    search.fit(x_train, y_train)
     return search.best_estimator_, search.best_score_, search.best_params_
