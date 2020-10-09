677bc0d26562957c6d681393b3ee6f106c68af4d
nidhal baccouri <nidhalbacc@gmail.com>
2020-10-09 23:23:38 +0200

    updatd hyperparams

--- igel/hyperparams.py
+++ igel/hyperparams.py
@@ -7,10 +7,19 @@ def hyperparameter_search(model,
                           x_train,
                           y_train,
                           **kwargs):
-    search = GridSearchCV(model,
-                          params,
-                          **kwargs) if method == 'grid_search' else RandomizedSearchCV(model,
-                                                                                       params,
-                                                                                       **kwargs)
+
+    search = None
+    if method == 'grid_search':
+        search = GridSearchCV(model,
+                              params,
+                              **kwargs)
+
+    elif method == 'random_search':
+        search = RandomizedSearchCV(model,
+                                    params,
+                                    **kwargs)
+    else:
+        raise Exception("hyperparameter method must be grid_search or random_search")
+
     search.fit(x_train, y_train)
     return search.best_estimator_, search.best_score_, search.best_params_
