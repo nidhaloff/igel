adade20247ac9df81b6588919e2f1f8ddee110ca
peter-shoes <bruce.mcculloch64@gmail.com>
2020-10-10 15:10:12 -0400

    cleaned up code w/ functions, 90% complete

--- igel/gui.py
+++ igel/gui.py
@@ -26,13 +26,56 @@ def readme():
             description += node.astext() + '\n'
         return description.encode('ascii', 'ignore').strip()
 
+def entry_str(name,col,row,set_def=None):
+    tk.Label(root, text=name).grid(column=col,row=row)
+    var = tk.StringVar()
+    var.set(set_def)
+    tk.Entry(root,textvariable=var).grid(column=col+1,row=row)
+    return var
+
+def entry_int(name,col,row,set_def=None):
+    tk.Label(root, text=name).grid(column=col,row=row)
+    var = tk.IntVar()
+    var.set(set_def)
+    tk.Entry(root,textvariable=var).grid(column=col+1,row=row)
+    return var
+
+def optionmenu(name,col,row,options):
+    tk.Label(root, text=name).grid(column=col,row=row)
+    var = tk.StringVar(root)
+    var.set(options[0])
+    tk.OptionMenu(root,var,*options).grid(column=col+1,row=row)
+    return var
+
+def checkbox_bool(name,col,row):
+    tk.Label(root, text=name).grid(column=col,row=row)
+    var = tk.BooleanVar()
+    var.set(False)
+    tk.Checkbutton(root,text='True',variable=var).grid(column=col+1,row=row)
+    return var
+
 def opvars():
-    global rowcount
-    global complete
-    op = opvar.get()
-    tk.Label(root,text=op).grid(column=0,row=rowcount)
-    tk.Entry(root).grid(column=1,row=rowcount)
-    rowcount +=1
+    global opvars_rowcount
+    global opvars_complete
+    op = opvar_var.get()
+    tk.Label(root,text=op).grid(column=0,row=opvars_rowcount)
+    var = tk.StringVar()
+    tk.Entry(root,textvariable=var).grid(column=1,row=opvars_rowcount)
+    opvars_rowcount +=1
+    opvars_complete.append([op,var])
+
+def param_grid_add_row():
+    global param_rowcount
+    global param_complete
+    global param_grid_no_cols
+    params_list = []
+    for col in range(param_grid_no_cols.get()):
+        var = tk.StringVar()
+        tk.Entry(root,textvariable=var).grid(column=7+col,row=param_rowcount)
+        params_list.append(var)
+    param_rowcount+=1
+    pass
+
 
 def main():
     global root
@@ -58,47 +101,43 @@ def main():
     for line in textbase[276:314]:
         read_data_options.append(line.split(':')[0].strip())
     tk.Label(root, text='Read Data Options:').grid(column=0,row=2)
-    global opvar
-    opvar = tk.StringVar(root)
-    opvar.set(read_data_options[0])
-    tk.OptionMenu(root,opvar,*read_data_options).grid(column=1,row=2)
-    global rowcount
-    rowcount = 3
+
+# i'm sorry this is a huge fucking mess
+    global opvar_var
+    opvar_var = tk.StringVar(root)
+    opvar_var.set(read_data_options[0])
+    tk.OptionMenu(root,opvar_var,*read_data_options).grid(column=1,row=2)
+    global opvars_rowcount
+    opvars_rowcount = 3
     tk.Button(root, text='Add Option',command=opvars).grid(column=2,row=2)
+    global opvars_complete
+    opvars_complete = []
 
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
     tk.Label(root, text='Split Options').grid(column=4,row=0)
 
-    tk.Label(root, text='Test Size').grid(column=4,row=1)
-    testsize_var = tk.StringVar()
-    tk.Entry(root,textvariable=testsize_var).grid(column=5,row=1)
+    split_test_size = entry_int('Test Size',4,1,0.2)
 
-    tk.Label(root, text='Shuffle').grid(column=4,row=2)
-    shuffle_var = tk.BooleanVar()
-    shuffle_var.set(False)
-    tk.Checkbutton(root,text='True',variable=shuffle_var).grid(column=5,row=2)
+    split_shuffle = checkbox_bool('Shuffle',4,2)
 
-    tk.Label(root, text='Stratify (seperate by comma)').grid(column=4,row=3)
-    stratify_var = tk.StringVar()
-    tk.Entry(root,textvariable=stratify_var).grid(column=5,row=3)
+    split_stratify = entry_str('Stratify (Seperate by comma)',4,3)
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
     tk.Label(root, text='Preprocess').grid(column=4,row=4)
 
-    tk.Label(root, text='Encoding').grid(column=4,row=5)
-    encoding_var = tk.StringVar(root)
-    encoding_var.set('oneHotEncoding')
-    tk.OptionMenu(root,encoding_var,'oneHotEncoding','labelEncoding').grid(column=5,row=5)
+    preprocess_missing_values_list = ['mean','median','most_frequent','constant']
+    preprocess_missing_values = optionmenu('Missing Values',4,5,preprocess_missing_values_list)
 
-    tk.Label(root, text='Scaling Method').grid(column=4,row=6)
-    scalemodel_var = tk.StringVar(root)
-    scalemodel_var.set('standard')
-    tk.OptionMenu(root,scalemodel_var,'standard','minmax').grid(column=5,row=6)
+    preprocess_encoding_type_list = ['oneHotEncoding','labelEncoding']
+    preprocess_encoding_type = optionmenu('Encoding Type',4,6,preprocess_encoding_type_list)
 
-    tk.Label(root, text='Scaling Target').grid(column=4,row=7)
-    scaletarget_var = tk.StringVar(root)
-    scaletarget_var.set('inputs')
-    tk.OptionMenu(root,scaletarget_var,'inputs','outputs','all').grid(column=5,row=7)
+    preprocess_scale_method_list = ['standard','minmax']
+    preprocess_scale_method = optionmenu('Scaling Method',4,7,preprocess_scale_method_list)
+
+    preprocess_scale_target_list = ['inputs','outputs','all']
+    preprocess_scale_target = optionmenu('Scaling Target',4,8,preprocess_scale_target_list)
 
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
@@ -122,7 +161,7 @@ def main():
         algo_options_clustering.append(temp_algo_list[2])
     tk.Label(root, text='Algorithm').grid(column=6,row=2)
     algo_var = tk.StringVar(root)
-    algo_var.set(algo_options[0])
+    # algo_var.set(algo_options[0])
     if modeltype_var.get()=='regression':
         algo_options=algo_options_regression
     elif modeltype_var.get()=='classification':
@@ -131,8 +170,42 @@ def main():
         algo_options=algo_options_clustering
     tk.OptionMenu(root,algo_var,*algo_options).grid(column=7,row=2)
 
+    model_arguments = entry_str('Arguments (Seperate by comma)',6,3)
+
+    model_use_cv_estimator = checkbox_bool('Use CV Estimator',6,4)
+
+    tk.Label(root, text='Cross Validate').grid(column=6,row=5)
+
+    model_cross_validate_cv = entry_int('CV',6,6,5)
+
+    model_cross_validate_n_jobs = entry_int('N Jobs',6,7)
+
+    model_cross_validate_verbose = entry_int('Verbose',6,8,0)
+
+    tk.Label(root,text='Hyperparameter Search').grid(column=6,row=9)
+
+    model_hyperparameter_search_method_list = ['grid_search','random_search']
+    model_hyperparameter_search_method = optionmenu('Method',6,10,model_hyperparameter_search_method_list)
+
+    tk.Label(root, text='Parameter Grid').grid(column=6,row=11)
+    global param_grid_no_cols
+    param_grid_no_cols = entry_int('Number of Columns',6,12,2)
+    tk.Button(root, text='Add Grid Row',command=param_grid_add_row).grid(column=8,row=12)
+    global param_rowcount
+    global param_complete
+    param_rowcount = 13
+
+
+
+
+
+
 
     root.mainloop()
+
+# fix this shit!!!!!!!!
+    for x in opvars_complete:
+        print('%s : %s'%(x[0],x[1].get()))
 
 
     opvars_dict = {}
@@ -145,9 +218,14 @@ def main():
     'split_shuffle':shuffle_var.get(),
     'split_stratify':stratify_var.get().split(','),
     'preprocess_missing_values': 'get',
-    'preprocess_encoding_tpye': encoding_var.get(),
+    'preprocess_encoding_type': encoding_var.get(),
     'preprocess_scale_method':scalemodel_var.get(),
-    'preprocess_scale_target':scaletarget_var.get()
+    'preprocess_scale_target':scaletarget_var.get(),
+    'model_type':modeltype_var.get(),
+    'model_algorithm':'get',
+    'model_arguments':args_var.get(),
+    'model_use_cv_estimator':cvestimator_var.get(),
+    'model_cross_validate_cv':cv_var.get()
     }
 
     print(yaml_dict)
