40d7171dee553c1cc848fffab681d3da442ddaeb
nidhal baccouri <nidhalbacc@gmail.com>
2020-10-31 00:07:37 +0100

    updated sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,7 @@ html_logo = '../assets/logo.jpg'
 # needs_sphinx = '1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.autosectionlabel', 'edx_theme'] # , 'sphinx_copybutton'
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.autosectionlabel'] # , 'sphinx_copybutton'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -85,7 +85,11 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'edx_theme'
+import sphinx_glpi_theme
+
+html_theme = "glpi"
+
+html_theme_path = sphinx_glpi_theme.get_html_themes_path()
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
