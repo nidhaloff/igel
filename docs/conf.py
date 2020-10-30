16dbfa1083e2711650415d10ac4fa0564a7574fc
nidhal baccouri <nidhalbacc@gmail.com>
2020-10-31 00:50:31 +0100

    fixed docs

--- docs/conf.py
+++ docs/conf.py
@@ -19,11 +19,10 @@
 #
 import os
 import sys
-
 sys.path.insert(0, os.path.abspath('..'))
 
 import igel
-import sphinx_glpi_theme
+
 # -- General configuration ---------------------------------------------
 
 
@@ -34,10 +33,7 @@ html_logo = '../assets/logo.jpg'
 # needs_sphinx = '1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc',
-              'sphinx.ext.viewcode',
-              'sphinx.ext.autosectionlabel',
-              'sphinx_glpi_theme'] # , 'sphinx_copybutton'
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.autosectionlabel'] # , 'sphinx_copybutton'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -89,11 +85,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-
-
-html_theme = "glpi"
-
-html_theme_path = sphinx_glpi_theme.get_html_themes_path()
+html_theme = 'classic'
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
