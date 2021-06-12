5d4c592374508d091fe5c74ce38a4c7e840c0599
nidhal baccouri <nidhalbacc@gmail.com>
2021-06-12 22:58:42 +0200

    fixed flask imports

--- igel/servers/flask_server.py
+++ igel/servers/flask_server.py
@@ -1,15 +1,17 @@
 import pandas as pd
 import logging
+from igel.utils import load_trained_model
 
 logger = logging.getLogger(__name__)
 
 try:
-    from flask import Flask, jsonify
+    from flask import Flask, jsonify, request
 except ImportError as err:
     logger.fatal(f"Make sure you install flask on your machine. You can do this by running pip install flask\n"
-                 f"Error Trace: {err}")
+                 f"Traceback:\n{err}")
 
 app = Flask(__name__)
+model = load_trained_model()
 
 
 @app.route('/predict', methods=['POST'])
@@ -17,5 +19,5 @@ def predict():
      json_ = request.json
      query_df = pd.DataFrame(json_)
      query = pd.get_dummies(query_df)
-     prediction = lr.predict(query)
+     prediction = model.predict(query)
      return jsonify({'prediction': list(prediction)})
