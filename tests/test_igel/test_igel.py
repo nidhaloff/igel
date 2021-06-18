594386cebfe51a5e0903890f57de1fde1cd7dffc
nidhal baccouri <nidhalbacc@gmail.com>
2021-06-18 18:14:33 +0200

    updated tests

--- tests/test_igel/test_igel.py
+++ tests/test_igel/test_igel.py
@@ -5,30 +5,8 @@
 import pytest
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-    See more at: http://doc.pytest.org/en/latest/fixture.html
+def test_fit():
     """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+    test the fit model functionality
+    """
     pass
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
-    pass
-
-
-def test_command_line_interface():
-    """Test the CLI."""
-    pass
-    # runner = CliRunner()
-    # result = runner.invoke(cli.main)
-    # assert result.exit_code == 0
-    # assert 'igel.cli.main' in result.output
-    # help_result = runner.invoke(cli.main, ['--help'])
-    # assert help_result.exit_code == 0
-    # assert '--help  Show this message and exit.' in help_result.output
