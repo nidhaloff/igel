239e524bc8f13c75ac117558db3d7b7d42d7214c
Sparsh Nagpal <50074241+0sparsh2@users.noreply.github.com>
2022-01-12 13:27:12 +0530

    Added the changes as Dotstring/comments according to guidelines for contribution

--- igel/data.py
+++ igel/data.py
@@ -77,8 +77,10 @@ from sklearn.utils.multiclass import type_of_target
 from igel.extras.kmedoids import KMedoids
 from igel.extras.kmedians import KMedians
 
+'''
+#CatBoost Importing
 from catboost import CatBoostClassifier, CatBoostRegressor
-
+'''
 
 import logging
 
@@ -253,12 +255,14 @@ models_dict = {
             "class": GradientBoostingRegressor,
             "link": "https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.GradientBoostingRegressor.html"
                     "#sklearn.ensemble.GradientBoostingRegressor"
-        },
-
-        "CatBoostRegressor": {
-            "class": CatBoostRegressor,
-            "link": "https://catboost.ai/en/docs/concepts/python-reference_catboostregressor"
         }
+
+        #CatBoost Regression Model
+        #,
+        #"CatBoostRegressor": {
+        #    "class": CatBoostRegressor,
+        #    "link": "https://catboost.ai/en/docs/concepts/python-reference_catboostregressor"
+        #}
     },
 
     "classification": {
@@ -409,13 +413,16 @@ models_dict = {
             "class": MultinomialNB,
             "link": "https://scikit-learn.org/stable/modules/generated/sklearn.naive_bayes.MultinomialNB.html"
                     "#sklearn.naive_bayes.MultinomialNB"
-        },
+        } 
 
-        "CatBoostClassifier":{
-            "class": CatBoostClassifier,
-            "link": "https://catboost.ai/en/docs/concepts/python-reference_catboostclassifier"
-        }
 
+        #Classification Model for CatBoost
+        #,
+        #"CatBoostClassifier":{
+        #    "class": CatBoostClassifier,
+        #    "link": "https://catboost.ai/en/docs/concepts/python-reference_catboostclassifier"
+        #}
+        
 
     },
 
